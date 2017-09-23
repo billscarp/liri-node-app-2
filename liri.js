431b6e0a3a0aeb2d51b256ddc7150207be3ade79
Jodi Diehl <29265610+jdiehl99@users.noreply.github.com>
2017-09-23 13:29:45 -0400

    did screen capture of tweets and spotify

--- liri.js
+++ liri.js
@@ -1,13 +1,34 @@
 var fs = require("fs");
+var searchTerm = "";
 
-var liriCommand = process.argv[3];
+var liriCommand = (process.argv[2]);
+// console.log(liriCommand);
+
+if (process.argv.length > 3) {
+    for (let i = 3; i < process.argv.length; i++) {
+        searchTerm += (process.argv[i] + "+");
+    }
+}
 
 // node liri.js my-tweets
 // This will show your last 20 tweets and when they were created at in your terminal/bash window.
 if (liriCommand === "my-tweets") {
+    var Twitter = require('twitter');
     var keys = require("./keys.js");
+    var client = new Twitter(keys);
 
+    var params = {
+        screen_name: 'LiriTweets1',
+        count: 20
+    };
 
+    client.get('statuses/user_timeline', params, function (error, data) {
+        if (error) throw error;
+
+        for (i = 0; i < data.length; i++) {
+            console.log(data[i].text);
+        }
+    });
 }
 
 // node liri.js spotify-this-song '<song name here>'
@@ -19,21 +40,46 @@ if (liriCommand === "spotify-this-song") {
     // The album that the song is from
     // If no song is provided then your program will default to "The Sign" by Ace of Base.
 
-    var Spotify = require('node-spotify-api');
 
-    var spotify = new Spotify({
-        id: "7d18c66395e84a77b70de7708bb4d3e1",
-        secret: "bb69571e30f4441387e936bfc09f927b"
+    if (searchTerm == "") {
+        var songSearch = "The Sign artist:Ace of Base";
+    } else {
+        var songSearch = '"'+searchTerm+'"';
+    }
+
+    var SpotifyWebApi = require('spotify-web-api-node');
+
+    var spotifyApi = new SpotifyWebApi({
+        clientId: '7d18c66395e84a77b70de7708bb4d3e1',
+        clientSecret: 'bb69571e30f4441387e936bfc09f927b',
     });
 
-    spotify
-        .request('https://api.spotify.com/v1/tracks/7yCPwWs66K8Ba5lFuU2bcx')
+    // Retrieve an access token
+    spotifyApi.clientCredentialsGrant()
         .then(function (data) {
-            console.log(data);
-        })
-        .catch(function (err) {
-            console.error('Error occurred: ' + err);
+            // console.log('The access token expires in ' + data.body['expires_in']);
+            // console.log('The access token is ' + data.body['access_token']);
+
+            // Save the access token so that it's used in future calls
+            spotifyApi.setAccessToken(data.body['access_token']);
+
+            // search tracks with song name in title
+            spotifyApi.searchTracks('track:' + songSearch, { limit: 1 })
+                .then(function (response) {
+                    // var songInfo = response.body.tracks.items;
+                    var songInfo = response.body.tracks.items[0];
+                    // console.log(songInfo);
+                    console.log("Artist:",songInfo.album.artists[0].name);
+                    console.log("Song:",songInfo.name);
+                    console.log("Preview link:",songInfo.preview_url);
+                    console.log("Album:",songInfo.album.name);
+                }, function (err) {
+                    console.log('Something went wrong!', err);
+                });
+        }, function (err) {
+            console.log('Something went wrong when retrieving an access token', err.message);
         });
+
 }
 
 // node liri.js movie-this '<movie name here>'
